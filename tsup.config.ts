c534a839d0e6d8f5d9a20659418e211012418d9d
BatuhanTopcu <batuhantopcu17@gmail.com>
2024-05-22 00:14:55 +0300

    setup project

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: false,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  target: "node14",
+  outDir: "dist",
+});
