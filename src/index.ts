73cffe06a59f18377a87fbbdad9320684da099fb
BatuhanTopcu <batuhantopcu17@gmail.com>
2024-05-22 01:52:24 +0300

    created base lib functions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 import consola from "consola";
 import { colors } from "consola/utils";
 import * as fs from "fs/promises";
+import { ComponentTypeOpts, Config, SelectedFilesOpts } from "./types.js";
+import { createFiles } from "./lib.js";
 
 consola.log({
   message:
@@ -21,27 +23,71 @@ consola.log({
     ),
 });
 
-const componentName = await consola.prompt(colors.bold("Enter component name:"), {
+const config: Config = {
+  componentName: "",
+  componentType: "server",
+  selectedFiles: [],
+};
+
+config.componentName = await consola.prompt(colors.bold("Enter component name:"), {
   type: "text",
   required: true,
 });
 
-// await consola.prompt(colors.bold("Select file(s) to create:"), {
-//   type: "multiselect",
-//   options: [],
-//   required: true,
-// });
+config.componentType = (
+  await consola.prompt(colors.bold("Select component type:"), {
+    type: "select",
+    options: [
+      {
+        label: "Server Component",
+        value: "server",
+      },
+      {
+        label: "Client Component",
+        value: "client",
+      },
+    ] as const satisfies ComponentTypeOpts,
+    required: true,
+  })
+).value;
+
+config.selectedFiles = (
+  await consola.prompt(colors.bold("Select additional file(s) to create:"), {
+    type: "multiselect",
+    options: [
+      {
+        label: `${config.componentName}.test.tsx`,
+        value: "test",
+      },
+
+      {
+        label: `${config.componentName}.module.scss`,
+        value: "scss",
+      },
+      {
+        label: `${config.componentName}.types.ts`,
+        value: "types",
+      },
+      {
+        label: `${config.componentName}.constants.ts`,
+        value: "constants",
+      },
+      {
+        label: `${config.componentName}.utils.ts`,
+        value: "utils",
+      },
+      {
+        label: `${config.componentName}.hooks.ts`,
+        value: "hooks",
+      },
+    ] as const satisfies SelectedFilesOpts,
+    initial: "test",
+    required: true,
+  })
+).map((option) => option.value);
 
 consola.start("Building component...");
 
-const directory = process.cwd();
-
-await fs.mkdir(`${directory}/${componentName}`);
-
-await fs.appendFile(`${directory}/${componentName}/index.ts`, "");
-await fs.appendFile(`${directory}/${componentName}/${componentName}.tsx`, "");
-await fs.appendFile(`${directory}/${componentName}/${componentName}.module.scss`, "");
-await fs.appendFile(`${directory}/${componentName}/${componentName}.types.ts`, "");
-await fs.appendFile(`${directory}/${componentName}/${componentName}.constants.ts`, "");
+await createFiles(config);
 
 consola.success("Created component!");
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type ComponentType = "server" | "client";
+
+export type FileType = "test" | "scss" | "types" | "constants" | "utils" | "hooks";
+
+export type Config = {
+  componentName: string;
+  componentType: ComponentType;
+  selectedFiles: Array<FileType>;
+};
+
+export type ComponentTypeOpts = Array<{
+  label: string;
+  value: ComponentType;
+}>;
+
+export type SelectedFilesOpts = Array<{
+  label: string;
+  value: FileType;
+}>;
+
+export type CreaterFunction = (config: Config) => Promise<void>;
